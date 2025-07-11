ec0f63c4132ebc9944bad304dabc80d325b473a0
Joe Doughty <Joseph.Doughty@Paconsulting.com>
2025-07-11 17:52:58 +0100

    Added light and dark mode

---
+++ src/app.tsx
@@ -0,0 +1,61 @@
+import { ColorSystemOptions, createTheme, ThemeOptions, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+import "./app.css";
+
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router
+    }
+}
+
+const lightPalette: ColorSystemOptions = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+};
+
+const darkPalette: ColorSystemOptions = {
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+};
+
+// const isDarkMode = true;
+// const theme = createTheme(
+//     (isDarkMode ? darkPalette : lightPalette)
+// );
+
+const theme = createTheme({
+    colorSchemes: {
+        dark: darkPalette,
+        light: lightPalette,
+    },
+});
+
+
+function App() {
+    return (<ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+    </ThemeProvider>)
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,41 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import App from "./App";
 
-import "./main.css";
 import "./styles.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-// TODO enable light mode
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#ff5252',
-    },
-  },
-});
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <React.StrictMode>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <App />
     </React.StrictMode>,
   )
 }
