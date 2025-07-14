22f1eae5c0d61056415a7b00919f7e927a02049f
Joe Doughty <Joseph.Doughty@Paconsulting.com>
2025-07-14 11:35:24 +0100

    WIP Added inital implementation for expandable table

--- src/routes/containers/list.tsx
+++ src/routes/containers/list.tsx
@@ -1,7 +1,8 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { invoke } from '@tauri-apps/api/core';
 import { useEffect, useState } from 'react';
 import { ContainerSummary } from '../../types/tauri/commands/docker/ContainerSummary';
+import ExpandableTable from '../../components/shared/table/expandableTable';
 
 export const Route = createFileRoute('/containers/list')({
   component: RouteComponent,
@@ -23,6 +24,28 @@ function RouteComponent() {
           <p>{containers.Names}</p>
         ))}
       </div>
+      {/* essert (100g serving)
+                              Calories
+                              Fat&nbsp;(g)
+                              Carbs&nbsp;(g)
+                              Protein&nbsp;(g) */}
+      <ExpandableTable columns={[
+        { name: "Calories" },
+        { name: "Fat", displayName: <>Fat&nbsp;(g)</> },
+        { name: "Carbs", displayName: <>Carbs&nbsp;(g)</> },
+        { name: "Protein", displayName: <>Protein&nbsp;(g)</> },
+        { name: "Price", displayName: <>Price&nbsp;(£)</> },
+      ]}
+        rows={[
+          {
+            Calories: 5,
+            Fat: 10,
+            Carbs: 186,
+            Protein: 15,
+            Price: 14.58,
+          }
+        ]}
+      />
     </section>
   )
 }
